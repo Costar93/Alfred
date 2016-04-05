af65071761dc5c544bd7b02f6b9443c4b6edb3ea
Costar93 <kisame_18@hotmail.com>
2016-04-05 15:55:53 +0200

    Primera estructura Alfred

--- Alfred/Alfred.py
+++ Alfred/Alfred.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Alfred(object):
+    """Class for Alfred Personal Digital Butler
+
+    Will run our house"""
+
+    def __init__(self):
+        super(Alfred, self).__init__()
+
+    def mainloop(self):
+        #while True:
+        #   command = get_command
+        #   do_command(command)
+        #   update
+        pass
+
 if __name__=="__main__":
     print "Here be dragons!"
