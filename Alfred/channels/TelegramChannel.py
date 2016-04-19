668f5791cc5a1720c9df1ebe7ce7ca25375dd888
Costar93 <kisame_18@hotmail.com>
2016-04-19 16:37:26 +0200

    Bot updated with response

--- Alfred/channels/TelegramChannel.py
+++ Alfred/channels/TelegramChannel.py
@@ -1,6 +1,29 @@
 from Channel import Channel
 import telepot
 
+class AlfredBot(telepot.Bot):
+    """AlfredBot is my telegram bot"""
+    def __init__(self, token):
+        super(AlfredBot, self).__init__(token)
+        self.clist = None
+        self.chat_id = None
+
+    def set_list(self, clist):
+        self.clist = clist
+        #clist perque no es pot utilitzar list
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == "text":
+            command = msg['text']
+            if self.clist is not None:
+                self.clist.append(command)
+                self.chat_id = chat_id
+
+    def respond(self, response):
+        if self.chat_id is not None:
+            self.sendMessage(self.chat_id,response)
+
 class TelegramChannel(Channel):
     """Channel class, recieved commands from Telegram"""
     def __init__(self, name="TelegramChannel"):
@@ -17,19 +40,7 @@ class TelegramChannel(Channel):
     def msg_avail(self):
         return len(self.messages) > 0
 
-class AlfredBot(telepot.Bot):
-    """AlfredBot is my telegram bot"""
-    def __init__(self, token):
-        super(AlfredBot, self).__init__(token)
-        self.clist = None
-
-    def set_list(self, clist):
-        self.clist = clist
-        #clist perque no es pot utilitzar list
-
-    def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        if content_type == "text":
-            command = msg['text']
-            if self.clist is not None:
-                self.clist.append(command)
+    def respond(self, response):
+        if response is None:
+            response = "Sorry Sir, I don't understand that command"
+        self.bot.respond(response)
